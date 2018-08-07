1e948e9f98815a950bcf05456bc7fc000f62a8fe
chinaxlt <wb-xlt390559@alibaba-inc.com>
2018-08-08 05:58:25 +0800

    add model

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2018/8/1 project create <br>
 2018/8/2 add server&server starter <br>
 2018/8/7 add mybatis <br>
+2018/8/8 add dubbo <br>
--- server/src/main/java/com/chinaxlt/study/service/impl/BasicServiceImpl.java
+++ server/src/main/java/com/chinaxlt/study/service/impl/BasicServiceImpl.java
@@ -1,12 +1,14 @@
 package com.chinaxlt.study.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.chinaxlt.study.dao.BasicDao;
 import com.chinaxlt.study.pojo.Basic;
 import com.chinaxlt.study.service.BasicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 @Service
+@Component
 public class BasicServiceImpl implements BasicService {
 
     @Autowired
